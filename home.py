8192a4b0771b3265c717b3c5b2b9d8abc2d35dbf
Alex Mineeva <amineeva@olin.edu>
2025-01-16 13:19:18 -0500

    Fixed redirects to home and map, flask going to correct pages now

--- home.py
+++ home.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template_string, render_template
+from flask import Flask, render_template_string, render_template, redirect
 
 app = Flask(__name__)
+
+# Redirect the root URL to /home
+@app.route("/")
+def root():
+    return redirect("/home", code=302)
 
 #home function at index.html
 @app.route("/home")
 def home():
     return render_template("index.html")
 
-@app.route("/map")
+@app.route("/index.html")
+def redirect_to_home():
+    return redirect("/home")
+
+@app.route("/MVP1map.html")
 def row():
     return app.send_static_file('MVP1map.html')
 
